502d5a45f00232bb4b44eabf3574e9d34dba47f4
cgmckeever <cgmckeever@gmail.com>
2018-06-03 11:10:21 -0500

    Update keymap.sh

--- keymap.sh
+++ keymap.sh
@@ -26,7 +26,7 @@ BTN_TL=lb,BTN_TR=rb,BTN_TL2=lt,BTN_TR2=rt \
         --ui-buttonmap tr=KEY_I \
         --ui-buttonmap lt+tl=KEY_H \
         --ui-buttonmap lb=KEY_KPMINUS,lt+lb=KEY_BACKSLASH \
-        --ui-buttonmap rb=KEY_BACKSPACE,lt+rb=KEY_KPMINUS \
+        --ui-buttonmap rb=KEY_BACKSPACE,lt+rb=KEY_KPPLUS \
         --ui-buttonmap lt+rt=KEY_U \
         --axismap -Y1=Y1,-Y2=Y2 \
         --ui-buttonmap rt=BTN_LEFT,b=BTN_RIGHT \
@@ -46,11 +46,12 @@ sudo /opt/retropie/supplementary/xboxdrv/bin/xboxdrv \
         --silent \
         --mimic-xpad \
         --evdev-absmap \
-ABS_X=x1,ABS_Y=y1,ABS_RX=x2,ABS_RY=y2,\
+ABS_X=x1,ABS_Y=y1,\
+ABS_Z=x2,ABS_RZ=y2,\
 ABS_HAT0X=dpad_x,ABS_HAT0Y=dpad_y,\
 ABS_BRAKE=lt,ABS_GAS=rt \
         --evdev-keymap \
-BTN_MODE=guide,BTN_SELECT=back,BTN_START=start,\
+KEY_HOMEPAGE=guide,BTN_BACK=back,BTN_START=start,\
 BTN_SOUTH=a,BTN_EAST=b,BTN_NORTH=x,BTN_WEST=y,\
 BTN_THUMBL=tl,BTN_THUMBR=tr,\
 BTN_TL=lb,BTN_TR=rb \
@@ -61,14 +62,14 @@ BTN_TL=lb,BTN_TR=rb \
         --ui-buttonmap guide=KEY_ENTER,lt+guide=KEY_M \
         --ui-buttonmap tr=KEY_I \
         --ui-buttonmap lt+tl=KEY_H \
-        --ui-buttonmap lb=KEY_KPMINUS,lt+lb=KEY_BACKSLASH \
-        --ui-buttonmap rb=KEY_BACKSPACE,lt+rb=KEY_KPMINUS \
-        --ui-axismap lt+rt=KEY_U \
+        --ui-buttonmap lb=KEY_KPMINUS,lt+lb=XK_BACKSLASH \
+        --ui-buttonmap rb=KEY_BACKSPACE,lt+rb=KEY_KPPLUS \
+        --ui-buttonmap lt+rt=KEY_U \
         --axismap -Y1=Y1,-Y2=Y2 \
-        --ui-axismap rt=BTN_LEFT \
+        --ui-buttonmap rt=BTN_LEFT \
         --ui-buttonmap b=BTN_RIGHT \
         --axis-sensitivity x1=1.0,y1=1.0 \
-        --ui-axismap x2=REL_X:20:3,y2=REL_Y:20:3 \
+        --ui-axismap x1^deadzone:2000=REL_X:35,y1^deadzone:2000=REL_Y:35 \
         --axis-sensitivity x2=0,y2=0 \
-        --ui-axismap x1^deadzone:2000=REL_X:14,y1^deadzone:2000=REL_Y:14 \
+        --ui-axismap x2=REL_X:8:5,y2=REL_Y:8:5 \
         &
